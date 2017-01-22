ea090a58d934e7c794feeaf04d727a85f556ad05
Tagore <tagore@hod.life>
2017-01-22 21:26:57 +0530

    adding language change

--- web/janaswaram/index.php
+++ web/janaswaram/index.php
@@ -349,7 +349,7 @@ function googleTranslateElementInit() {
  <div class="row">
           <div class="col-md-12">
              <select id="drpLanguage"
-            onchange="javascript:changeLanguage(this.options[this.selectedIndex].value);)" 
+            onchange="javascript:changeLanguage(this.options[this.selectedIndex].value);" 
             name="drpLanguage" title="Choose Language" class="big pull-right">
             <option value='pramukhindic:telugu'>Telugu</option>
             <option value=":english" selected="selected">English</option>
