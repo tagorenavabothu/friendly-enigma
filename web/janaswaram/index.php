1ecca901f4ba536c3fbefbd12b8b12fbc6ebd56e
Tagore <tagore@hod.life>
2017-01-22 18:21:22 +0530

    adding language change

--- web/janaswaram/index.php
+++ web/janaswaram/index.php
@@ -324,25 +324,9 @@ body {
         <div class="row">
           <div class="col-md-12">
             <div id="google_translate_element"></div><script type="text/javascript">
- function googleTranslateElementInit() {
-    new google.translate.TranslateElement({  
-      pageLanguage: 'en', 
-      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-    }, 'google_translate_element');
-  }
-</script>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-<script>
-  $(document).ready(function(){
-    $('#google_translate_element').bind('DOMNodeInserted', function(event) {
-      $('.goog-te-menu-value span:first').html('Translate');
-      $('.goog-te-menu-frame.skiptranslate').load(function(){
-        setTimeout(function(){
-          $('.goog-te-menu-frame.skiptranslate').contents().find('.goog-te-menu2-item-selected .text').html('Translate');    
-        }, 100);
-      });
-    });
-  });
+function googleTranslateElementInit() {
+ new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
           </div>
 
