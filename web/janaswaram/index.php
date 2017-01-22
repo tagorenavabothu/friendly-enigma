8c5c983075ee8552b1fe5efc762e04d1e2ab29cf
Tagore <tagore@hod.life>
2017-01-22 20:48:26 +0530

    adding language change

--- web/janaswaram/index.php
+++ web/janaswaram/index.php
@@ -1,8 +1,10 @@
 <?php
 //namespace MicrosoftAzure\Storage\Common;
 
-ini_set('upload_max_filesize','30M');
-ini_set('post_max_size','30M');
+ini_set('upload_max_filesize', '10M');
+ini_set('post_max_size', '10M');
+ini_set('max_input_time', 300);
+ini_set('max_execution_time', 300);
 
 require 'PHPMailer/PHPMailerAutoload.php';
 
