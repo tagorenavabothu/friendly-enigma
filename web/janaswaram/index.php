5a494b2262486b19829de091ad564e28ab5b5872
Tagore <tagore@hod.life>
2017-01-22 21:23:41 +0530

    adding language change

--- web/janaswaram/index.php
+++ web/janaswaram/index.php
@@ -538,7 +538,7 @@ else
       pramukhIME.setLanguage(lang[1], lang[0]);
      // var ul = document.getElementById('pi_tips');
 
-      var elem, len = ul.childNodes.length, i;
+      /*//var elem, len = ul.childNodes.length, i;
       for (i = 0; i < len; i++) {
           elem = ul.childNodes[i];
           if (elem.tagName && elem.tagName.toLowerCase() == 'li') {
@@ -547,7 +547,7 @@ else
       }
       for (i = len - 1; i > 1; i--) {
           ul.removeChild(ul.childNodes[i]);
-      }
+      }*/
      // ul.childNodes[i].className = 'tip'; // replace small tip text with large
 
       //showNextTip(); // call for first time
